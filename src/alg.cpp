5508b801f29f10403c764634252b2a4feb712dd9
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-02-27 20:00:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,9 +5,9 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
-  if (value == 0 && value == 1) {
+  if ((value == 0) or (value == 1)) {
     return false;
-  } else if (value != 0 && value != 1) {
+  } else if ((value != 0) or (value != 1)) {
     for (int i = 2; i < value; i++) {
       if (value % i == 0) {
         return false;
