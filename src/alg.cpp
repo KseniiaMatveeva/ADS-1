c9c0afffe7262b90ede469c110f00982f6a6bc68
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-02-27 20:24:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -35,12 +35,12 @@ uint64_t nPrime(uint64_t n) {
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
   value++;
-  for (uint64_t i = value; i++); {
+  for (uint64_t i = value; ; i++) {
     if (checkPrime(i)) {
       return i;
     }
   }
-  return i;
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
