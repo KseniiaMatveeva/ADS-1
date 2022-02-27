4d808b89dccaedd5f18f98d36b4d42856b831ae5
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-02-27 15:33:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,7 +8,7 @@ bool checkPrime(uint64_t value) {
   if (value == 0 && value == 1) {
     return false;
   } else if (value != 0 && value != 1) {
-    for (int i = 2; i <= value; i++) {
+    for (int i = 2; i < value; i++) {
       if (value % i == 0) {
         return false;
       } else {
@@ -24,14 +24,16 @@ uint64_t nPrime(uint64_t n) {
   int k = 1;
   uint64_t value = 0;
   while (k <= n) {
-    if (k = n) {
+    if (k = 1) {
       return 2;
-    } else {
+    } else if (k < n) {
       if (checkPrime(value)) {
         k++;
         value++;
       } else {
         value++;
+      } else if (k = n) {
+        return value;
       }
     }
   }
@@ -50,7 +52,7 @@ uint64_t nextPrime(uint64_t value) {
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
   uint64_t sum = 0;
-  uint64_t value = 2;
+  uint64_t value = 0;
   while (value < hbound) {
     if (checkPrime(value)) {
       sum = sum + value;
