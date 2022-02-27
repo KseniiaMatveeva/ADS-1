52468c1802c789e8118a308002152ff8831f9e72
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-02-27 21:54:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,19 +5,18 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
-  /*if (value == 0 || value == 1) {
+  if (value == 0 || value == 1) {
     return false;
-  } else */
-  if (value > 1) {
-    for (int i = 2; i < value; i++) {
-      if (value % i == 0) {
-        return false;
-      } else {
-          return true;
+  } else if (value > 1) {
+      for (int i = 2; i < value; i++) {
+        if (value % i == 0) {
+          return false;
+        } else {
+            return true;
+        }
       }
     }
-  }
-  return true;
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
